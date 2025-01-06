6b8286ed115abfde44ee99a67e4a9064dd91afcd
Sam Park <sam.park7029@gmail.com>
2025-01-06 03:38:00 -0500

    Updated main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+from utils import read_video, save_video
+from trackers import Tracker
+import cv2
+from team_assigner import TeamAssigner
+from player_ball_assigner import PlayerBallAssigner
+
+def main():
+    # Read Video
+    video_frames = read_video('input_videos/08fd33_4.mp4')
+
+    # Initialize Tracker
+    tracker = Tracker('models/best.pt')
+
+    tracks = tracker.get_object_tracks(video_frames, 
+                                       read_from_stub=True, 
+                                       stub_path='stubs/track_stubs.pkl')
+
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+    
+    # Assign Ball Aquisition
+    player_assigner = PlayerBallAssigner()
+    for frame_num, player_track in enumerate(tracks['players']):
+        ball_bbox = tracks['ball'][frame_num][1]['bbox']
+        assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+
+        if assigned_player != -1:
+            tracks['players'][frame_num][assigned_player]['has_ball'] = True
+
+    # Draw Output
+    # Draw Object Tracks 
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+
+    # Save Video
+    save_video(output_video_frames, 'output_videos/output_video.avi') 
+
+if __name__ == '__main__':
+    main()
